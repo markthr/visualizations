9b0c276e4162160e6ca770ca92700b6d10d63a5b
Mark <43383309+markthr@users.noreply.github.com>
2025-02-19 19:40:44 -0500

    Added figure to show launch angle dependence
    Figure shows that across the range of launch angles for a barreled ball hit at 100 mph, the error in the model (almost certainly from the stop condition) is bigger than the sensitivity of a given launch angle to gravitational difference.
    
    Next revision will have tweaks to the baseball dynamics models to account for this.

--- baseball/generate_figures_gravity.py
+++ baseball/generate_figures_gravity.py
@@ -5,6 +5,7 @@ from baseball_dynamics import distance_no_drag
 from baseball_dynamics import distance_quad_drag
 
 
+
 #--------------------------------------------------------------
 #   Computer how an example barreled ball travels in different
 #   ballparks without air resistance and with equalized air
@@ -21,6 +22,7 @@ parks[bbd_vac_col] = parks.apply(lambda row: distance_no_drag(row[grav_col])[0],
 parks[bbd_qd_col] = parks.apply(lambda row: distance_quad_drag(row[grav_col])[0], axis=1)
 
 
+# Fig: rav_comparison_no_drag.png
 # Generate figure for ball without drag
 fig, ax = plt.subplots()
 ax.barh(parks.index, parks[bbd_vac_col])
@@ -32,6 +34,7 @@ ax.grid(visible=True)
 fig.set_size_inches(20.5, 10.5)
 fig.savefig("baseball/figs/grav_comparison_no_drag.png", dpi=100)
 
+# Fig: grav_comparison_quad_drag.png
 # Generate figure for ball with drag
 fig, ax = plt.subplots()
 ax.barh(parks.index, parks[bbd_qd_col])
@@ -42,3 +45,37 @@ ax.set_title("Barreled ball distance by park with quadratic air resistance")
 ax.grid(visible=True)
 fig.set_size_inches(20.5, 10.5)
 fig.savefig("baseball/figs/grav_comparison_quad_drag.png", dpi=100)
+
+# Fig: grav_and_launch_angle_dependence_qd.png
+# Generate figure to show how launch angle affects dependence of distance on gravity
+labels = ["min", "mean", "max"]
+colors = ["green", "dodgerblue", "orange"]
+g_vals = parks[grav_col].describe()[labels].values
+# range of launch angles for a ball hit at 100 mph to be conssidered a barrel
+launch_angles = np.linspace(24, 33, num = (33-24)*2+1) 
+distances = np.zeros([g_vals.size, launch_angles.size])
+
+for i, g_val in enumerate(g_vals):
+    for j, angle in enumerate(launch_angles):
+        distances[i, j] = distance_quad_drag(g_val, launch_angle=angle)[0]
+
+fig, axs = plt.subplots(2, 1, sharex=True)
+for i, row in enumerate(distances):
+    axs[0].scatter(launch_angles, row, c=colors[i], label=f"{labels[i]}={g_vals[i]:.3f}")
+# subtract mean from min and max to plot the difference
+distances[0, :] -= distances[1, :]
+distances[2, :] -= distances[1, :]
+axs[1].scatter(launch_angles, distances[0, :], c = colors[0])
+axs[1].scatter(launch_angles, distances[2, :], c = colors[2])
+
+for ax in axs:
+    ax.grid(visible=True)
+    ax.set_xlim(left= launch_angles[0] -0.1, right=launch_angles[-1] +0.1)
+    ax.set_xticks(ticks=np.arange(launch_angles[0], launch_angles[-1]+1))
+axs[0].set_ylabel("Distance (feet)")
+axs[1].set_ylabel("Difference from Mean(feet)")
+axs[1].set_xlabel("Launch Angle (degrees)")
+axs[0].legend()
+fig.set_size_inches(20.5, 10.5)
+plt.subplots_adjust(hspace=0)
+fig.savefig("baseball/figs/grav_and_launch_angle_dependence_qd.png", dpi=100)
